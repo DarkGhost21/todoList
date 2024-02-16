1af4e2982f195b9e12b00853d235e71cf5c2c1b8
DerekMelendez <derekobied21@gmail.com>
2024-02-16 16:26:39 -0600

    Inicio de sesion nuevo

---
+++ README.md
@@ -0,0 +1,6 @@
+Cuando quieras usar este proyecto:
+1. Crear una carpeta
+2. Crear un entorno virtual usando "py -3 -m venv {nombre del entorno virtual} "
+3. Activar entorno virtual usando ".{nombre del entorno virtual}\Scripts\activate"
+4. Installar flask usando "pip install Flask"
+5. Correr app con: "flask --app hello --debug run"
---
+++ todor/auth.py
@@ -0,0 +1,56 @@
+from flask import (
+    Blueprint, render_template, request, url_for, redirect, flash, session, g
+    )
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .models import User
+from todor import db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods = ('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User(username, generate_password_hash(password))
+
+        error = None
+
+        user_name = User.query.filter_by(username = username).first()
+        if user_name == None:
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        else:
+            error = f'El usuario {username} ya esta registrado'
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods = ('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        error = None
+        #Validar datos
+        user = User.query.filter_by(username = username).first
+        if user == None:
+            error = 'Nombre de usuario incorrecto'
+        elif not check_password_hash(user.password, password):
+            error = 'Contraseña incorrecta'
+
+        #Iniciar sesion
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('todo.index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
